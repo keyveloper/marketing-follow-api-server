632c2aef0671e2aa89a7fabad332adff294d8d4a
zen <didehdrb21@naver.com>
2025-07-20 20:56:22 +0900

    follow-api-server build start!

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "marketing-follow-api-server"
---
+++ src/main/kotlin/org/example/marketingfollowapiserver/MarketingFollowApiServerApplication.kt
@@ -0,0 +1,11 @@
+package org.example.marketingfollowapiserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarketingFollowApiServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<MarketingFollowApiServerApplication>(*args)
+}
